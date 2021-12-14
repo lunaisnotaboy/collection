b45f64e7742a655b36a8f778f4c41a64bdaaf211
Luna <stampylongr7+luna@gmail.com>
2021-12-14 17:34:11 +0000

    Add stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <Tutorial />
+  <div class="bg-gray-100 flex items-top justify-center min-h-screen relative sm:items-center sm:pt-0">
+    <div class="max-w-4xl mx-auto lg:px-8 sm:px-6">
+      <NuxtLink class="flex justify-center pt-8 sm:pt-0" to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16" fill="none" height="87" viewBox="0 0 296 87" width="296">
+          <path d="M102 27.476h8.755l18.556 29.6674V27.476h8.082v42.0374h-8.696l-18.615-29.6058v29.6058H102V27.476Zm71.519 42.0372h-7.531v-4.6956c-1.715 3.3045-5.204 5.1651-9.612 5.1651-6.981 0-11.514-4.9245-11.514-12.0119V39.2441h7.532v17.1772c0 4.1438 2.573 7.0258 6.43 7.0258 4.225 0 7.164-3.243 7.164-7.6862V39.2441h7.531v30.2691Zm26.21-.4785-7.226-10.0897-7.227 10.0897h-8.022L188.4 53.2398l-10.287-14.4742h8.204l6.186 8.7104 6.122-8.7104h8.267l-10.35 14.4742 11.146 15.7949h-7.959Zm23.268-38.9182v9.127h8.572V45.43h-8.572v15.0259c0 .5047.204.9887.567 1.3458.364.3571.857.5581 1.372.5589h6.633v7.1402h-4.961c-6.735 0-11.143-3.8445-11.143-10.5651V45.4359h-6.062v-6.1924h3.803c1.774 0 2.807-1.0359 2.807-2.7498v-6.3772h6.984Zm27.559 29.8765V27.476h8.079v30.0254c0 8.1087-3.98 12.012-10.715 12.012h-9.405v-7.1403h9.614c.644 0 1.261-.2508 1.716-.6971.455-.4464.711-1.0517.711-1.683Zm12.921-4.5934h7.78c.431 4.6956 3.614 7.6861 8.758 7.6861 4.593 0 7.78-1.799 7.78-5.5232 0-9.0097-23.04-2.7645-23.04-18.8587C264.76 31.7983 270.823 27 279.533 27c9.064 0 15.186 5.1035 15.56 12.9716h-7.738c-.365-3.6626-3.291-6.1248-7.837-6.1248-4.225 0-6.858 1.9222-6.858 4.8629 0 9.309 23.34 2.3478 23.34 18.7384C296 65.2545 289.447 70 280.018 70c-9.737 0-16.104-5.7668-16.532-14.5945" fill="#012a35" />
+          <path clip-rule="evenodd" d="M39.115 19.0524c-2.2607-3.8871-7.9124-3.8871-10.1731 0L4.30396 61.4154c-2.26069 3.8871.56516 8.7459 5.08654 8.7459h19.2337c-1.9321-1.6885-2.6475-4.6094-1.1855-7.1155l18.6598-31.9859-6.9835-12.0075Z" fill="#80eec0" fill-rule="evenodd" />
+          <path d="M54.726 28.3449c1.8709-3.1803 6.5482-3.1803 8.4191 0l20.39 34.6606c1.871 3.1804-.4677 7.1558-4.2095 7.1558H38.5455c-3.7418 0-6.0805-3.9754-4.2095-7.1558l20.39-34.6606Z" fill="#00dc82" />
+        </svg>
+      </NuxtLink>
+
+      <div class="bg-white mt-8 overflow-hidden p-6 shadow sm:rounded-lg">
+        <h2 class="font-semibold leading-7 text-2xl text-center">
+          My Personal Collection of Files
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Hello! If you've stumbled upon this page, you must be lost.
+          This website is for storing all of my files that should be
+          <span class="font-bold text-gray-900">publicly accessible</span>.
+          (i.e. files not relating to any specific things, images for
+          chatrooms, etc.)
+        </p>
+        <p class="mt-3 text-gray-600">
+          If you want to take a look around, click on the button below!
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
